4d0cfd9a3b5760a5efeb5a60ed61f7d9c65ff88c
salvador <salvador.cmenendez@gmail.com>
2019-10-07 21:31:09 -0500

    html components

--- hello-world.py
+++ hello-world.py
@@ -1,9 +1,15 @@
 import dash
-from dash_html_components import H1
+import dash_html_components as html
 
 app = dash.Dash(__name__)
-app.title = "Hola Uma"
+app.title = "UMA Finance"
 
-app.layout = H1("Hola amigos de la UMA!")
+app.layout = html.Div([
+    html.H1("Información financiera al instante"),
+    html.P("""
+    En esta aplicación creada por actuarios de la UMA podrás consultar información financiera
+    directo de Yahoo Finance y visualizarla al instante.
+    """)
+])
 
 app.server.run(debug=True)
