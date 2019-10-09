ec300742f49351dd4f3237f3b759a71136468d3c
salvador <salvador.cmenendez@gmail.com>
2019-10-08 22:14:41 -0500

    callbacks

--- hello-world.py
+++ hello-world.py
@@ -2,7 +2,8 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import plotly.graph_objects as go
-
+from dash.dependencies import Input, Output
+from pandas_datareader import data
 app = dash.Dash(__name__)
 app.title = "UMA Finance"
 
@@ -29,4 +30,27 @@ app.layout = html.Div([
     dcc.Graph(figure=fig, id="plot_security")
 ])
 
-app.server.run(debug=True)
+@app.callback(
+    Output("plot_security", "figure"),
+    [Input("security", "value")]
+)
+def YahooQuery(sec):
+    try:
+        info = data.DataReader(sec, start='2017-1-1', data_source='yahoo')
+        DATA = go.Scatter(x=info.index, y=info["Adj Close"])
+        layout={
+            "title":f"Historico de precios para {sec}",
+            "xaxis":{"title":"Fecha"},
+            "yaxis":{"title":"Precio"}
+        }
+    except:
+        DATA= go.Scatter(x=list(range(1,10)), y=[10]*9),
+        layout={
+            "title":f"No se encontró info para {sec}",
+            "xaxis":{"title":"Semestre"},
+            "yaxis":{"title":"Calificación"}
+        }
+    return go.Figure(data=DATA, layout=layout)
+
+if __name__=="__main__":
+    app.server.run(debug=True)
